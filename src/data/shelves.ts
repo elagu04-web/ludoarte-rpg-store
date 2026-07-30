b9d6668d2d44c636c13b045063451aaeb56a303a
Ludoarte <elagu04@gmail.com>
2026-07-30 00:12:42 -0300

    Agrega la caja real de Dados Pato Mar (version transparente)

--- src/data/shelves.ts
+++ src/data/shelves.ts
@@ -61,7 +61,7 @@ export const shelves: ShelfData[] = [
         duration: "15 min",
       },
       { id: "niji", name: "Niji", price: 1250, stock: 1, image: "/assets/boardgames/niji.png" },
-      { id: "dados-pato-mar", name: "Dados Pato Mar", price: 890, stock: 1, image: PLACEHOLDER_IMAGE },
+      { id: "dados-pato-mar", name: "Dados Pato Mar", price: 890, stock: 1, image: "/assets/boardgames/dados-pato-mar.png" },
     ],
   },
   {
